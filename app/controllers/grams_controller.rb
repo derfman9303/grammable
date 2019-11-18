ae3a20b152cfe3a82a1aec14253524a4354091b7
derfman9303 <frederichodges@gmail.com>
2019-11-18 22:16:29 +0000

    added edit and update fonctionality

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,8 +1,24 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def index
 
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -19,15 +35,17 @@ class GramsController < ApplicationController
   end
 
   def show
-    @gram = Gram.find_by_id(params[:id]) 
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
 
   private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
