b1cd5f08ce60aeba2d1306f7d700430450d48b09
derfman9303 <frederichodges@gmail.com>
2019-11-18 20:11:51 +0000

    added factorybot

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
+  gem "factory_bot_rails"
 end
 gem 'jquery-rails'
 gem 'popper_js', '~> 1.11.1'
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -13,11 +13,7 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
     it "should successfully show the new form" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
       get :new
       expect(response).to have_http_status(:success)
@@ -29,11 +25,7 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: 'Hello!' } }
@@ -44,11 +36,7 @@ RSpec.describe GramsController, type: :controller do
       expect(gram.user).to eq(user)
     end
     it "should properly deal with validation errors" do 
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       gram_count = Gram.count
