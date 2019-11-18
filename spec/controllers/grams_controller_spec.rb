ad8b86e65886c5d97f72b5a45eb506145bcde18d
derfman9303 <frederichodges@gmail.com>
2019-11-18 20:31:30 +0000

    backup

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page is the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#index action" do
     it "should successfully show the page" do 
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
   describe "grams#new action" do
     it "should require users to be logged in" do
       get :new
@@ -19,6 +32,7 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
   describe "grams#create action" do 
     it "should require users to be logged in" do
       post :create, params: { gram: { message: "Hello" } }
@@ -45,4 +59,5 @@ RSpec.describe GramsController, type: :controller do
       expect(gram_count).to eq Gram.count
     end
   end
+
 end
