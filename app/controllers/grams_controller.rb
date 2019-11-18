239ff7e0449da609b3bc7824ba0736d34d5d2009
derfman9303 <frederichodges@gmail.com>
2019-11-18 20:52:31 +0000

    added a detail page for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,15 +4,24 @@ class GramsController < ApplicationController
   def index
 
   end
+
   def new
     @gram = Gram.new
   end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id]) 
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
     end
   end
 
